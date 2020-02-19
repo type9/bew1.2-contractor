4c241e2be1f253b52a350ef46a477adb01443074
MackRoe <elaine.music@students.makeschool.com>
2020-02-18 20:11:17 -0800

    clean up over-length lines in urls.py

--- rideit/rideshare/urls.py
+++ rideit/rideshare/urls.py
@@ -9,16 +9,22 @@ Abbreviation Key:
 
 urlpatterns = [
     path('', CommunityListView.as_view(), name='community-list-page'),
-    path('cm/<str:slug>/', CommunityDetailView.as_view(), name='community-details-page'),
-    path('createcommunity/', CommunityCreateView.as_view(), name='community-create-page'),
+    path('cm/<str:slug>/', CommunityDetailView.as_view(),
+         name='community-details-page'),
+    path('createcommunity/', CommunityCreateView.as_view(),
+         name='community-create-page'),
 
-    path('rs/<int:pk>', RideShareDetailView.as_view(), name='rideshare-details-page'),
-    path('cm/<str:slug>/newrs', RideShareCreateView.as_view(), name='rideshare-create-page'),
+    path('rs/<int:pk>', RideShareDetailView.as_view(),
+         name='rideshare-details-page'),
+    path('cm/<str:slug>/newrs', RideShareCreateView.as_view(),
+         name='rideshare-create-page'),
 
     # blacklist message route
-    path('cm/blacklist/<str:slug>', BlacklistView.as_view(), name='blacklist-page'),
+    path('cm/blacklist/<str:slug>', BlacklistView.as_view(),
+         name='blacklist-page'),
 
     # add blocked user to blacklist
-    path('cm/blacklist/<str:slug>/<int:pk>', BlockUser, name='blacklist-page'),
+    path('cm/blacklist/<str:slug>/<int:pk>', BlockUser,
+         name='blacklist-page'),
 
 ]

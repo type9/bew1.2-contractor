e1717f5f8de2a3d9163e84e2eb66dbeb0d4f4f0e
type9 <gabespersonal@gmail.com>
2020-03-03 18:11:11 -0800

    Fixes

--- rideit/rideit/settings.py
+++ rideit/rideit/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -91,11 +93,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
-    'default': {
-         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-         'NAME': 'geodjango',
-         'USER': 'rideit',
-    },
+    'default': {dj_database_url.config(conn_max_age=600)},
 }
 
 

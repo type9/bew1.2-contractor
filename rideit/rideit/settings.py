d972b94d012c17613811930eed76f0cf961484dc
type9 <gabespersonal@gmail.com>
2020-03-03 18:14:49 -0800

    Fixes

--- rideit/rideit/settings.py
+++ rideit/rideit/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
-    'default': {dj_database_url.config(conn_max_age=600)},
+    'default': dj_database_url.config(conn_max_age=600),
 }
 
 
